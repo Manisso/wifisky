cd339b40f8d104007d9ee429def36ab1e7644ff7
Manisso <manis98@live.com>
2019-08-19 00:45:00 +0100

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,50 @@
+#Greet's To
+#IcoDz - Canejo              
+#Tool For Hacking 
+#Authors : Manisso
+
+clear
+
+sudo chmod +x /etc/
+
+clear
+
+sudo chmod +x /usr/share/doc
+
+clear
+
+sudo rm -rf /usr/share/doc/wifisky/
+
+clear
+
+cd /etc/
+
+clear
+
+sudo rm -rf /etc/Manisso
+
+clear
+
+mkdir Manisso
+
+clear
+
+cd Manisso
+
+clear
+
+git clone https://github.com/Manisso/wifisky.git
+
+clear
+
+cd wifisky
+
+clear
+
+sudo chmod +x install.sh
+
+clear
+
+./install.sh
+
+clear
