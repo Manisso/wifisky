971829a7eec09541f9a305b5d643123e78df3a68
Manisso <manis98@live.com>
2019-08-19 04:11:12 +0100

    Update wifisky.py

--- wifisky.py
+++ wifisky.py
@@ -21,7 +21,6 @@ follow = """
 
 #Reaver implements a brute force attack against Wifi Protected Setup (WPS) registrar PINs in order to recover WPA/WPA2 
 #Reaver has been designed to be a robust and practical attack against WPS, and has been tested against a wide variety of access points and WPS implementations.
-#On average Reaver will recover the target AP’s plain text WPA/WPA2 passphrase in 4-10 hours, depending on the AP. In practice, it will generally take half this time to guess the correct WPS pin and recover the passphrase
 #Reaver Homepage | Kali Reaver Repo
 #Author: Tactical Network Solutions, Craig Heffner
 ##License: GPLv2
